af4cbd14907e86c1fdf3e2485d3fc10c17d28323
ElMelo9 <efmelo9@gmail.com>
2024-11-05 17:01:21 -0500

    services get all dinamico

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { ServicioService } from '../services/servicio/servicio.service';
+import { TipoServicioService } from '../services/tipoServicio/tipo-servicio.service';
+import { EstadoServicioService } from '../services/estadoServicio/estado-servicio.service';
+import { EstadoServicioResponse } from '../interfaces/estadoServicio.inteface';
+import { TipoServicioResponse } from '../interfaces/tipoServicio.interface';
+import { getFromLocalStorage } from '../helpers/storage-helper';
+import { ServicioResponse } from '../interfaces/servicio.interface';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +14,81 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  estadoServicio: EstadoServicioResponse[] = [];
+  tipo: TipoServicioResponse[] = [];
+  idUsuario!: number
+  servicios: ServicioResponse[] = [];
+
+
+  constructor(
+    private estadoService: EstadoServicioService,
+    private tipoServicio: TipoServicioService,
+    private servicio: ServicioService
+  ) {}
+
+  ngOnInit() {
+    this.idUsuario = getFromLocalStorage('id_usuario')
+    this.estadoGetAll();
+    this.tipoServicioGetAll();
+    this.getServicios(this.idUsuario)
+
+  }
+
+  estadoGetAll() {
+    this.estadoService.getAll().subscribe({
+      next: (response: EstadoServicioResponse[]) => {
+        //console.log(response);
+        this.estadoServicio = response
+
+      },
+      error: (error) => {
+        console.error('Error al obtener los estados', error);  // Manejar el error aquí
+      },
+      complete: () => {
+
+      }
+    });
+  }
+
+  getServicios(id_usuario: number){
+    this.servicio.getByUsuario(id_usuario).subscribe({
+      next: (response: ServicioResponse[]) => {
+        //console.log(response);
+        this.servicios = response
+
+      },
+      error: (error) => {
+        console.error('Error al obtener los servicios del usuario', error);  // Manejar el error aquí
+      },
+      complete: () => {
+
+      }
+    });
+
+  }
+
+  tipoServicioGetAll() {
+
+    this.tipoServicio.getAll().subscribe({
+      next: (response: TipoServicioResponse[]) => {
+        //console.log(response);
+        this.tipo = response
+
+      },
+      error: (error) => {
+        console.error('Error al obtener los tipo de servicios ', error);  // Manejar el error aquí
+      },
+      complete: () => {
+
+      }
+    });
+
+  }
+
+  getNombreServicio(id: number): string | undefined {
+    const servicio = this.tipo.find(s => s.id_tipo_servicio === id);
+    return servicio ? servicio.nombre_tipo_servicio : 'Servicio no encontrado';
+  }
 
   
 
